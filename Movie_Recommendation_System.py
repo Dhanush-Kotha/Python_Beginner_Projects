9d7ddc51565a3def8b4b58fc91777810163368dc
Dhanush Kotha <kpurnasarvesh@gmail.com>
2026-09-15 23:53:08 +0530

    Update movie recommendation system to use online dataset

--- Movie_Recommendation_System.py
+++ Movie_Recommendation_System.py
@@ -7,7 +7,8 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
 #1.Load movie dataset
-movies=pd.read_csv("movies.csv")
+url = "https://raw.githubusercontent.com/Dhanush-Kotha/Python_Beginner_Projects/main/movies.csv"
+movies = pd.read_csv(url)
 
 #2.Clean missing values
 movies['genres']=movies['genres'].fillna("")
